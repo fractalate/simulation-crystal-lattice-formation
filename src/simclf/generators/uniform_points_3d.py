a20e5461bbc3bb4530f02f0eaf2ba2cabce98b5a
Kate Novak <kje@cochleoid.com>
2026-02-16 08:18:43 -0500

    Better Random Points Implementation

--- src/simclf/generators/uniform_points_3d.py
+++ src/simclf/generators/uniform_points_3d.py
@@ -4,14 +4,8 @@ from simclf.typing import Point3D, Point3DArray
 
 
 def generate_uniform_points_3d(extents_a: Point3D, extents_b: Point3D, count: int) -> Point3DArray:
-    parts = []
-
-    for i in range(3):
-        low, high = min(extents_a[i], extents_b[i]), max(extents_a[i], extents_b[i])
-        parts.append(np.random.uniform(
-            low=low,
-            high=high,
-            size=count,
-        ))
-
-    return np.stack(parts).T
+    return np.random.uniform(
+        low=extents_a,
+        high=extents_b,
+        size=(count, 3),
+    )
