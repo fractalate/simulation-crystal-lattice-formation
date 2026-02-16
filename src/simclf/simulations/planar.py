1828c1bdb900f7d1aa1864ac59735323461fc588
Kate Novak <kje@cochleoid.com>
2026-02-16 07:20:14 -0500

    A Planar Sample, Some Touch Ups in 3D

--- src/simclf/simulations/planar.py
+++ src/simclf/simulations/planar.py
@@ -11,7 +11,7 @@ ATTRACTIVE_FACTOR = 3.6e-28
 REPULSIVE_FACTOR = 1.0e-95
 REPULSIVE_POWER = 8.0
 
-NUMBER_OF_ATOMS = 75
+NUMBER_OF_ATOMS = 100
 
 equilibrium_spacing = calculate_equilibrium_spacing(
     attractive_factor=ATTRACTIVE_FACTOR,
@@ -20,21 +20,12 @@ equilibrium_spacing = calculate_equilibrium_spacing(
 )
 print(f"{equilibrium_spacing=}")
 
-# XXX math problem: given a XxYxZ space which we'll fill with uniformly sized spheres having radius r,
-# what is that chance that uniformly adding N spheres to the space will produce a configuration with overlapping spheres?
-# Or maybe it's more interesting to ask: given that space, how many overlapping spheres can we expect when adding N spheres?
-# With this, I can generate N+fudge atoms for the simulation where fudge is this number.
-# Then I can remove any overlapping and hopefully we're left with about N atoms.
-
-# But for now we just put some atoms in for now and remove those which overlap.
-
-# We'll simulate in a cube, so we roughly cube root N atoms along each axis.
-# Scale with some fudge so it's less likely atoms will touch.
 simulation_dimension = 1.0 * NUMBER_OF_ATOMS**(1/3) * (2 * equilibrium_spacing) * .2
 
+# force the atoms into a planar arrangement
 atom_positions = generate_uniform_points_3d(
-    np.array([-simulation_dimension / 2.0, -simulation_dimension / 2.0, -simulation_dimension / 2.0]),
-    np.array([ simulation_dimension / 2.0,  simulation_dimension / 2.0,  simulation_dimension / 2.0]),
+    np.array([-simulation_dimension / 2.0, -simulation_dimension / 2.0, 0]),
+    np.array([ simulation_dimension / 2.0,  simulation_dimension / 2.0, 0]),
     NUMBER_OF_ATOMS,
 )
 
@@ -42,12 +33,16 @@ atom_positions = generate_uniform_points_3d(
 class DoItAgain(Exception):
     pass
 
+# Detect atoms which overlap too much. Atoms are allowed to overlap considerably, but not
+# too much. This allows for some portion of the points to spread out, rather than contract,
+# which numerically helps reach a solution faster as the gradient when atoms are overlapping
+# is much steeper than when the yare spaced apart.
 while True:
     try:
         for i in range(atom_positions.shape[0] - 1):
             for j in range(i + 1, atom_positions.shape[0]):
                 distance = np.linalg.norm(atom_positions[i] - atom_positions[j])
-                if distance < equilibrium_spacing / 5.0: # XXX kindof let the overlap, but you don't want too much since repulsive force is so powerful
+                if distance < equilibrium_spacing / 5.0:
                     atom_positions[j] = atom_positions[atom_positions.shape[0] - 1]
                     atom_positions = atom_positions[:-1,]
                     raise DoItAgain()
@@ -74,20 +69,17 @@ for k in range(40):
     for i in range(atom_positions.shape[0]):
         for j in range(atom_positions.shape[1]):
             mask[i][j] = step_size
-            temp0 = calculate_potential_energy_of_basic_system(
-                atom_positions=atom_positions - mask / 2.0,
+            temp = calculate_potential_energy_of_basic_system(
+                atom_positions=atom_positions + mask,
                 attractive_factor=ATTRACTIVE_FACTOR,
                 repulsive_factor=REPULSIVE_FACTOR,
                 repulsive_power=REPULSIVE_POWER,
             )
-            temp1 = calculate_potential_energy_of_basic_system(
-                atom_positions=atom_positions + mask / 2.0,
-                attractive_factor=ATTRACTIVE_FACTOR,
-                repulsive_factor=REPULSIVE_FACTOR,
-                repulsive_power=REPULSIVE_POWER,
-            )
-            gradient[i][j] = temp1 - temp0
+            gradient[i][j] = temp - potential_energy_of_system
             mask[i][j] = 0
+
+    # Don't accept movement along the Z axis.
+    gradient[:,2] = 0
 
     scale_factor = np.linalg.norm(gradient.flatten())
     if scale_factor > 0.0:
@@ -112,8 +104,6 @@ for k in range(40):
                 potential_energy_of_system = potential_energy_of_system_next
             else:
                 break
-
-#print(atom_positions)
 
 xs = atom_positions[:, 0]
 ys = atom_positions[:, 1]
