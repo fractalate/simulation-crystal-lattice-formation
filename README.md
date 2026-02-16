b7bbd183d983b2a77be9787bf7d91034712b20dc
Kate Novak <kje@cochleoid.com>
2026-02-16 11:50:02 -0500

    Some Cleanup

--- README.md
+++ README.md
@@ -1,26 +1,8 @@
 # Simulation of Crystal Lattice Formation
 
-## Thoughts
-
-*February 12, 2026*
-
-I want to make this simulation and in a conversation with ChatGPT about my troubles with naive simulations using a more classical approach, it recommended me to use an approach which attempts to minimize the potential in the group of atoms. I think I can give this an honest shot, I just need to take inventory of some things I understand:
-
-* For some parameters $A$, $B$, and $n$ related to the ionic system and $r$ being the distance from the atom, we have
-* Attractive energy $E_A = - \frac{A}{r}$.
-* Repulsive energy $E_R = \frac{B}{r^n}$.
-* Values for $n$ are around $8$.
-* Source: Callister and Rethwish - Material Science and Engineering: An Introduction - 8e - Chapter 2.6
-
-My initial thoughts on an approach is to randomly place some atoms in space, then consider the gradient of the potential field at each atom to determine which direction it needs to move to lower its potential.
-
-Let $E(\textbf{x}_j) = \sum_{i \ne j} \frac{B}{ | \textbf{x}_j - \textbf{x}_i |^n} - \frac{A}{ | \textbf{x}_j - \textbf{x}_i | } $ be the potential of the atom at position $\textbf{x}_j$.
-
-Then $U(\textbf{x}_1, \dots, \textbf{x}_m) = \sum_{i < j} E(\textbf{x}_j)$ is the potential function for the whole system. This can be considered as a function of a single vector stacking the coordinates of each $\textbf{x}_j$ into one vector. Then we can take the gradient of $U$ and engage with minimization to find a local minimum.
-
 ## Setup
 
-```
+```bash
 # Set up a virtual env for dependencies.
 python3 -m venv --prompt simclf .venv
 
@@ -30,3 +12,11 @@ source .venv/bin/activate
 # Install dependencies.
 pip install --editable .
 ```
+
+## Running Basic Simulation
+
+```bash
+python3 -m simclf.simulations.basic
+```
+
+This will output files in a directory roughly named like `out/{timestamp}_basic_{version}`.
--- src/simclf/simulations/basic.py
+++ src/simclf/simulations/basic.py
@@ -1,5 +1,20 @@
-import os
-import time
+"""
+TODO Write this up better and rename the simulation to something else. Then update README.md.
+
+I want to make this simulation and in a conversation with ChatGPT about my troubles with naive simulations using a more classical approach, it recommended me to use an approach which attempts to minimize the potential in the group of atoms. I think I can give this an honest shot, I just need to take inventory of some things I understand:
+
+* For some parameters $A$, $B$, and $n$ related to the ionic system and $r$ being the distance from the atom, we have
+* Attractive energy $E_A = - \\frac{A}{r}$.
+* Repulsive energy $E_R = \\frac{B}{r^n}$.
+* Values for $n$ are around $8$.
+* Source: Callister and Rethwish - Material Science and Engineering: An Introduction - 8e - Chapter 2.6
+
+My initial thoughts on an approach is to randomly place some atoms in space, then consider the gradient of the potential field at each atom to determine which direction it needs to move to lower its potential.
+
+Let $E(\\textbf{x}_j) = \\sum_{i \\ne j} \\frac{B}{ | \\textbf{x}_j - \\textbf{x}_i |^n} - \\frac{A}{ | \\textbf{x}_j - \\textbf{x}_i | } $ be the potential of the atom at position $\\textbf{x}_j$.
+
+Then $U(\\textbf{x}_1, \\dots, \\textbf{x}_m) = \\sum_{i < j} E(\\textbf{x}_j)$ is the potential function for the whole system. This can be considered as a function of a single vector stacking the coordinates of each $\\textbf{x}_j$ into one vector. Then we can take the gradient of $U$ and engage with minimization to find a local minimum.
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,6 +23,8 @@ from simclf.generators.uniform_points_3d import generate_uniform_points_3d
 from simclf.physics.atom import calculate_equilibrium_spacing
 from simclf.physics.energy import calculate_potential_energy_of_basic_system
 from simclf.writer import Writer
+
+writer = Writer("basic", "1.0.0")
 
 
 # XXX these magic numbers give like a 3 angstrom equilibrium spacing which is in the ballpark for some atomic configurations.
@@ -23,8 +40,6 @@ equilibrium_spacing = calculate_equilibrium_spacing(
     repulsive_power=REPULSIVE_POWER,
 )
 print(f"{equilibrium_spacing=}")
-
-writer = Writer("basic", "1.0.0")
 
 writer.write_object_to_json("config.json", {
     "number_of_atoms": NUMBER_OF_ATOMS,
